c7836740aa68060ded71a08206d544356941fe26
JaredMel <jareddmelendez1764@gmail.com>
2024-03-08 14:03:29 -0800

    WorkinProgress

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
    N2 = N;
-   for (size_t i = 0; i < M-1; i++)
+   for (size_t i = 0; i < M; i++)
    {
       if (i == 0)
       {
